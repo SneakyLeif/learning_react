8c6bfe099da2aa14954c65532ea1c564a7700cfa
Sneaky <SneakyLeif1020@gmail.com>
2019-11-23 21:11:50 -0500

    big update

--- js/script.js
+++ js/script.js
@@ -1,58 +1,124 @@
 'use strict';
 
+var mainMenu;
+var titleBar;
+
 function load() {
     document.getElementById("loading-div").style.display = 'none';
 
-    var element = (
-        <div id="titlebar" className='columns'>
-            <div className='column col-1'></div>
-            <div id='navbar' className='column col-10'>
-                <div className='text-center'>
-                    <img src='img/logo.png' style={{"height": "80px", "position": "relative", "top": "-4px"}}></img>
-                </div>
-            </div>
-            <div className='column col-1'></div>
-        </div>
-    );
-    ReactDOM.render(element, document.querySelector('#navbar-container'));
+    var TitleBar = posed.div({
+        on: {y: 0},
+        off: {y: -40}
+    });
+    var TitleBarLogo = posed.img({
+        on: {y: 0, delay: 100},
+        off: {y: -40, delay: 100}
+    });
+    class TopBar extends React.Component {
+        state = { isOn: false };
 
-    var element = (
-        <div>
-            <div className='columns'>
-                <div className='column col-12 text-center'>
-                    <h1>DCI Login</h1>
-                </div>
-            </div>
+        constructor(props) {
+            super(props);
+        }
 
-            <div className='columns'>
-                <div className='column col-12 text-center'>
-                    <input id='login-username-input' className='form-input p-centered' type='text' placeholder='Username'></input>
-                    <input id='login-password-input' className='form-input p-centered' type='password' placeholder='Password'></input>
-                </div>
-            </div>
+        componentDidMount() {
+            this.toggle();
+        }
 
-            <br/>
+        toggle = () => this.setState({ isOn: !this.state.isOn });
 
-            <div className='columns'>
-                <div className='column col-12 text-center'>
-                    <div id='login-button-container'>login</div>
-                    <div id='login-register-button-container'>register</div>
-                </div>
-            </div>
-        </div>
-    );
-    ReactDOM.render(element, document.querySelector('#main-container'));
+        render() {
+            const { isOn } = this.state;
+        
+            return (
+                <TitleBar id="titlebar" className='columns' pose={isOn ? 'on' : 'off'}>
+                    <div className='column col-1'></div>
+                    <div id='navbar' className='column col-10'>
+                        <div className='text-center'>
+                            <TitleBarLogo src='img/logo.png' style={{"height": "80px", "position": "relative", "top": "16px"}}></TitleBarLogo>
+                        </div>
+                    </div>
+                    <div className='column col-1'></div>
+                </TitleBar>
+            );
+            
+        }
+    }
+    titleBar = ReactDOM.render(<TopBar />, document.querySelector('#navbar-container'));
+
+    var Menu = posed.div({
+        on: { staggerChildren: 50 }
+    });
+    var Item = posed.div({
+        on: { y: 0, x: 0, opacity: 1 },
+        off: { y: -10, x: -10, opacity: 0 }
+    });
+    class MainMenu extends React.Component {
+        state = { isOn: false };
+
+        constructor(props) {
+            super(props);
+        }
+    
+        toggle = () => this.setState({ isOn: !this.state.isOn });
+    
+        render() {
+            const { isOn } = this.state;
+        
+            return (
+                <Menu id={this.props.id} pose={isOn ? 'on' : 'off'}>
+                    <Item className='columns item'>
+                        <div className='column col-12 text-center'>
+                            <div className='text-container p-centered tooltip tooltip-right' data-tooltip="Why are you hovering over me? \n Go away">
+                                <h1 id='dci-login-text'>DCI Login</h1>
+                            </div>
+                        </div>
+                    </Item>
+
+                    <Item className='columns item'>
+                        <div className='column col-12 text-center'>
+                            <div className='text-input-container p-centered tooltip tooltip-left' data-tooltip="Username goes here">
+                                <input id='login-username-input' className='form-input' type='text' placeholder='Username'></input>
+                            </div>
+                        </div>
+                    </Item>
+                    <Item className='columns item'>
+                        <div className='column col-12 text-center'>
+                            <div className='text-input-container p-centered tooltip tooltip-left' data-tooltip="Password goes here">
+                                <input id='login-password-input' className='form-input' type='password' placeholder='Password'></input>
+                            </div>
+                        </div>
+                    </Item>
+
+                    <br/>
+
+                    <Item className='columns item'>
+                        <div className='column col-12 text-center'>
+                            <div id='login-button-container'></div>
+                        </div>
+                    </Item>
+                    <Item className='columns item'>
+                        <div className='column col-12 text-center'>
+                            <div id='login-register-button-container'></div>
+                        </div>
+                    </Item>
+                </Menu>
+            );
+        }
+    }
+    mainMenu = ReactDOM.render(<MainMenu id='mainMenu' />, document.querySelector('#main-container'));
+    setTimeout(() => mainMenu.toggle(), 200);
 
     class LoginButton extends React.Component {
         render() {
-            return (<button className="btn btn-primary" onClick={login}>Log In</button>);
+            return (<button id='login-button' className="btn btn-primary tooltip" data-tooltip="Click to sign in!" onClick={login}>Log In</button>);
         }
     }
     ReactDOM.render(React.createElement(LoginButton), document.querySelector('#login-button-container'));
 
     class LoginRegisterButton extends React.Component {
         render() {
-            return (<button className="btn mt-2" onClick={loginRegister}>Register</button>);
+            return (<button id='login-register-button' className="btn mt-2 tooltip tooltip-bottom" data-tooltip="Click to sign up!" onClick={loginRegister}>Register</button>);
         }
     }
     ReactDOM.render(React.createElement(LoginRegisterButton), document.querySelector('#login-register-button-container'));
@@ -60,70 +126,18 @@ function load() {
 load();
 
 function login() {
-    var user = document.getElementById("login-username-input").value;
-    var pass = document.getElementById("login-password-input").value;
-    console.log(user);
+    // Add loading animation to button
+    document.getElementById("login-button").classList.replace("tooltip", "loading");
+
+    var data = {
+        user: document.getElementById("login-username-input").value,
+        pass: document.getElementById("login-password-input").value
+    };
+    console.log(data.user);
+
+    socket.emit("submit-login", data);
 }
 
 function loginRegister() {
-
+    
 }
-
-/*
-var clicks = 0;
-var stored = 0;
-
-function refresh() {
-    ReactDOM.render(<div>Clicks: {clicks}</div>, document.querySelector('#click_count_container'));
-    ReactDOM.render(<div>Stored: {stored}</div>, document.querySelector('#stored_count_container'));
-}
-refresh();
-
-class ClickButton extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    clickButton() {
-        clicks++;
-        refresh();
-    }
-
-    render() {
-        return (
-            <button class="btn" onClick={() => this.clickButton()}>
-                Increment
-            </button>
-        );
-    }
-}
-ReactDOM.render(React.createElement(ClickButton), document.querySelector('#click_button_container'));
-
-class StoreButton extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    clickButton() {
-        if (clicks > 0) {
-            socket.emit("storeClicks", clicks);
-        }
-    }
-
-    render() {
-        return(
-            <button class="btn" onClick={() => this.clickButton()}>
-                Store
-            </button>
-        )
-    }
-}
-ReactDOM.render(React.createElement(StoreButton), document.querySelector('#store_button_container'));
-
-socket.on("storeResponse", function(data) {
-    if (data.status) {
-        clicks -= data.storedClicks;
-        stored = data.totalStored;
-        refresh();
-    }
-});*/
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var bcrypt = require('bcrypt');
 app.use(express.static('./'));
 
 var users = {}; // Stores all user data
+var uIndex = {};
 var customers = {}; // Stores all customer data
 var work_orders = {};
 var inventory = {};
@@ -52,16 +53,325 @@ function handleDisconnect() {
 }
 handleDisconnect(); // Connect and start disconnect handler
 
+// Loading users
+con.query("SELECT * FROM users", function(err, res) {
+	if (err) throw err;
 
+	for (var i= 0; i < res.length; i++) {
+		users[res[i].id] = createUserObj(res[i]);
+		uIndex[res[i].user.toLowerCase()] = res[i].id; // create uIndex reference for user
+	}
 
+	log("users loaded!", "green");
+});
+// Create user object
+function createUserObj(res) {
+	var user = JSON.parse( JSON.stringify( res ) );
+	delete user.id;
 
+	user.online = false;
 
- io.on('connection', function(socket) {
+	return user;
+}
+
+// Loading customers
+con.query("SELECT * FROM customers", function(err, res) {
+	if (err) throw err;
+
+	for (var i= 0; i < res.length; i++) {
+		customers[res[i].id] = createCustomerObj(res[i]);
+	}
+
+	log("customers loaded!", "green");
+});
+// Create customer object
+function createCustomerObj(res) {
+	var cust = JSON.parse( JSON.stringify( res ) );
+	delete cust.id;
+
+	return cust;
+}
+
+io.on('connection', function(socket) {
 
 	log("A user has connected.");
 	
+	// User Registration
+	socket.on('submit-register', function(regData) {
+		if (regData != "" && regData != null) {
+			var email = {
+				valid: false,
+				tooLong: false,
+				missing: false
+			};
+			var user = {
+				valid: false,
+				empty: false,
+				taken: false,
+				tooShort: false,
+				tooLong: false,
+				missing: false
+			};
+			var pass = {
+				valid: false,
+				empty: false,
+				matches: false,
+				tooShort: false,
+				tooLong: false,
+				missing: false,
+				missing_confirmation: false
+			};
+			
+			// Email
+			if (regData.hasOwnProperty("email")) {
+				if (regData.email == "") {
+					email.valid = true;
+					
+				} else {
+					if (regData.email.length <= 32) {
+						email.valid = true;
+						
+					} else {
+						email.tooLong = true;
+						
+					}
+				}
+			} else {
+				email.missing = true;
+			}
+			
+			// Username
+			if (regData.hasOwnProperty("user")) {
+				if (regData.user != "") {
+					if (!isUserTaken(regData.user)) {
+						if (regData.user.length >= 4) {
+							if (regData.user.length <= 18) {
+								user.valid = true;
+							} else {
+								user.tooLong = true;
+							}
+						} else {
+							user.tooShort = true;
+						}
+					} else {
+						user.taken = true;
+					}
+				} else {
+					user.empty = true;
+				}
+			} else {
+				user.missing = true;
+			}
+			
+			// Password
+			if (regData.hasOwnProperty("pass1")) {
+				if (regData.hasOwnProperty("pass2")) {
+					if (regData.pass1 != "") {
+						if (regData.pass1.length >= 5) {
+							if (regData.pass1.length <= 24) {
+								pass.valid = true;
+								
+								if (regData.pass1 == regData.pass2) {
+									pass.matches = true;
+									
+								}
+							} else {
+								pass.tooLong = true;
+								
+							}
+						} else {
+							pass.tooShort = true;
+							
+						}
+					} else {
+						pass.empty = true;
+						
+					}
+				} else {
+					pass.missing_confirmation = true;
+					
+				}
+			} else {
+				pass.missing = true;
+				
+			}
+			
+			if (email.valid && user.valid && pass.valid && pass.matches) {
+				console.log("someone tried to make an account with the username: \""+ regData.user +"\"");
+				
+				// CREATE ACCOUNT HERE!! (post to mysql, update users and uIndex, log user in)
+				
+				bcrypt.hash(regData.pass1, 10, function(err, hash) {
+					var insert = {
+						user: regData.user,
+						email: regData.email,
+						pass: hash
+					};
 
- });
+					con.query('INSERT INTO users SET ?', insert, function(err, res) {
+						if (err) throw err;
+						
+						users[res.insertId] = createUserObj(res);
+						uIndex[insert.user.toLowerCase()] = res.insertId;
+						socket.uId = res.insertId;
+
+						logIn(socket.uId);
+					});
+				});
+				
+			} else {
+				var err = {err: true};
+
+				for (var key in email) {
+					if (email[key]) {
+						err.email = key;
+					}
+				}
+
+				for (var key in user) {
+					if (user[key]) {
+						err.user = key;
+					}
+				}
+
+				for (var key in pass) {
+					if (pass[key]) {
+						err.pass = key;
+					}
+				}
+				if (!pass.matches) {
+					err.pass.matches = false;
+				}
+				
+				socket.emit('register-response', err);
+			}
+		}
+	});
+
+	// User Login
+	socket.on('submit-login', function(logData) {
+		if (logData != "" && logData != null) {
+			var user = {
+				id: 0,
+				valid: false,
+				empty: false,
+				missing: false
+			};
+			var pass = {
+				valid: false,
+				empty: false,
+				missing: false
+			}
+			var thePointOfNoReturn = false;
+
+			if (logData.hasOwnProperty("user")) {
+				if (logData.user != "") {
+					if (uIndex.hasOwnProperty(logData.user.toLowerCase())) {
+						user.valid = true;
+						user.id = uIndex[logData.user.toLowerCase()];
+						console.log(uIndex);
+						
+						if (logData.hasOwnProperty("pass")) {
+							if (logData.pass != "" && logData.pass != undefined) {
+								thePointOfNoReturn = true;
+
+								bcrypt.compare(logData.pass, users[user.id].pass, function(err, res) {
+									if (err) throw err;
+
+									if (res) {
+										pass.valid = true;
+										logIn(user.id);
+									}
+								});
+							} else {
+								pass.empty = true;
+							}
+						} else {
+							pass.missing = true;
+						}
+					}
+				} else {
+					user.empty = true;
+				}
+			} else {
+				user.missing = true;
+			}
+
+			if (!thePointOfNoReturn) {
+				if (user.valid && pass.valid) {
+					logIn(user.id);
+				} else {
+					var err = {err: true};
+
+					for (var key in user) {
+						if (user[key]) {
+							err.user = key;
+						}
+					}
+
+					for (var key in pass) {
+						if (pass[key]) {
+							err.pass = key;
+						}
+					}
+					if (!err.hasOwnProperty("pass")) {
+						err.pass = "not valid";
+					}
+
+					socket.emit('login-response', err);
+				}
+			}
+		}
+	});
+
+	function logIn(id) {
+		if (users.hasOwnProperty(id)) {
+			socket.pId = id;
+
+			io.in("p-"+ id).clients((err, clients) => {
+				if (clients.length == 0) {
+					socket.join("p-"+ id);
+				}
+			});
+
+			users[id].online = true;
+
+			data = {
+				status: true
+			};
+
+			socket.emit('login-response', data);
+
+			log (users[id].user +" has logged in!", "green");
+		} else {
+			log("error logging in :(", "red");
+		}
+	}
+	
+	socket.on('disconnect', function() {
+		if (socket.hasOwnProperty("pId")) {
+			users[socket.pId].online = false;
+
+			users[socket.pId].last_online = Date.now();
+			var sql = "UPDATE users SET last_online = '"+ users[socket.pId].last_online +"' WHERE id ='"+ socket.pId +"'";
+			con.query(sql, function(err, result) {if (err) throw err});
+
+			log(users[socket.pId].user +" has gone offline :(", "gray");
+		} else {
+			log("A user has disconnected :(", "gray");
+		}
+	});
+});
+
+function isUserTaken(user) {
+	taken = false;
+
+	if (uIndex.hasOwnProperty(user.toLowerCase())) {
+		taken = true;
+	}
+	
+	return taken
+}
 
 http.listen(port, () => console.log('Example app listening on port '+ port +'!'));
 
